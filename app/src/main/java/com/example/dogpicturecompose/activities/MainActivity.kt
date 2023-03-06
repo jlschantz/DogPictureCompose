04629223b60aac4995d5a32d2061d6ed49524c97
Jacob Schantz <jlschantz@hotmail.com>
2023-03-05 21:28:39 -0800

    change kotlin version

--- app/src/main/java/com/example/dogpicturecompose/activities/MainActivity.kt
+++ app/src/main/java/com/example/dogpicturecompose/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.dogpicturecompose.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dogpicturecompose.ui.theme.DogPictureComposeTheme
+import com.example.dogpicturecompose.viewmodels.DogViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val dogViewModel: DogViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
