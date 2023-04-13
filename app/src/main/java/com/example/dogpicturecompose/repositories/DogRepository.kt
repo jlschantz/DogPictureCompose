18c46cbd3cb1b9bdeb4487f19de6cec013f0a7c4
Jacob Schantz <jlschantz@hotmail.com>
2023-04-13 14:57:39 -0700

    changed repository function

--- app/src/main/java/com/example/dogpicturecompose/repositories/DogRepository.kt
+++ app/src/main/java/com/example/dogpicturecompose/repositories/DogRepository.kt
@@ -1,13 +1,23 @@
 package com.example.dogpicturecompose.repositories
 
 import com.example.dogpicturecompose.api.DogApi
-import com.example.dogpicturecompose.api.DogPictureResponse
-import retrofit2.Response
+import com.example.dogpicturecompose.api.ResultState
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class DogRepository @Inject constructor(private val dogApi: DogApi) {
 
-    suspend fun getDogPicturesByType(type: String): Response<DogPictureResponse>{
-        return dogApi.getDogPicturesByType(type)
+    fun getDogPicturesByType(type: String) = flow {
+        emit(ResultState.Loading())
+        try {
+            val response = dogApi.getDogPicturesByType(type)
+            if (response.isSuccessful) {
+                emit(ResultState.Success(response.body()?.message))
+            }else {
+                emit(ResultState.Error(null))
+            }
+        } catch (exception: Exception) {
+            emit(ResultState.Error(exception.localizedMessage))
+        }
     }
 }
--- app/src/main/java/com/example/dogpicturecompose/viewmodels/DogViewModel.kt
+++ app/src/main/java/com/example/dogpicturecompose/viewmodels/DogViewModel.kt
@@ -10,7 +10,6 @@ import com.example.dogpicturecompose.api.ResultState.*
 import com.example.dogpicturecompose.repositories.DogRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -20,23 +19,9 @@ class DogViewModel @Inject constructor(private val dogRepository: DogRepository)
 
     var resultState: ResultState<*> by mutableStateOf(Success<List<String>>(listOf()))
 
-    private fun getDogPicturesByType(type: String) = flow {
-        emit(Loading())
-        try {
-            val response = dogRepository.getDogPicturesByType(type)
-            if (response.isSuccessful) {
-                emit(Success(response.body()?.message))
-            }else {
-                emit(Error(null))
-            }
-        } catch (exception: Exception) {
-            emit(Error(exception.localizedMessage))
-        }
-    }
-
     fun searchForType(type: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            getDogPicturesByType(type).collect { resource ->
+            dogRepository.getDogPicturesByType(type).collect { resource ->
                 withContext(Dispatchers.Main) {
                     when (resource) {
                         is Success -> {
