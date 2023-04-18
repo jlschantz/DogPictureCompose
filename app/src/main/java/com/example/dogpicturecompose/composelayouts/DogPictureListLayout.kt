ea6ffee2a8b468a4e9de6dcdbf98a371965825d9
Jacob Schantz <jlschantz@hotmail.com>
2023-04-18 11:12:08 -0700

    moved dropdown menu

--- app/src/main/java/com/example/dogpicturecompose/composelayouts/DogPictureListLayout.kt
+++ app/src/main/java/com/example/dogpicturecompose/composelayouts/DogPictureListLayout.kt
@@ -2,19 +2,19 @@ package com.example.dogpicturecompose.composelayouts
 
 import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,63 +34,100 @@ fun DogPictureListLayout(dogViewModel: DogViewModel) {
             mutableStateOf(false)
         }
 
+        var showDropDown by remember {
+            mutableStateOf(false)
+        }
+
         if(searchBarDisplayed){
             SearchAppBar(
                 onSearchButtonClicked = { query ->
+                    showDropDown = false
                     dogViewModel.searchForType(query)
                 },
                 onCloseIconClicked = {
                     searchBarDisplayed = false
                 },
-                onSearchDogBreeds = { query ->
-                    dogViewModel.getDogBreedsStartingWith(query)
-                },
-                filteredDogBreedList = dogViewModel.filteredDogBreedList
+                onValueChange = { query ->
+                    showDropDown = query.isNotEmpty()
+                    if(showDropDown){
+                        dogViewModel.getDogBreedsStartingWith(query)
+                    }
+                }
             )
         } else {
-            TopAppBar(onSearchIconClicked = {
-                searchBarDisplayed = true
-            }
+            TopAppBar(
+                onSearchIconClicked = {
+                    searchBarDisplayed = true
+                }
             )
         }
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
         ) {
-            if (dogViewModel.searchForTypeResult is ResultState.Loading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(50.dp),
-                    color = Purple_500
-                )
-            } else {
-                val dogPictureList: List<*>? = dogViewModel.searchForTypeResult.data as? List<*>
-
-                if (dogPictureList.isNullOrEmpty()) {
-                    Text(
-                        text = stringResource(id = R.string.no_results),
-                        fontSize = 14.sp,
-                        color = Color.Gray
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (dogViewModel.searchForTypeResult is ResultState.Loading) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(50.dp),
+                        color = Purple_500
                     )
                 } else {
-                    LazyColumn {
-                        items(dogPictureList) { item ->
-                            PictureRow(url = item as String)
+                    val dogPictureList: List<*>? = dogViewModel.searchForTypeResult.data as? List<*>
+
+                    if (dogPictureList.isNullOrEmpty()) {
+                        Text(
+                            text = stringResource(id = R.string.no_results),
+                            fontSize = 14.sp,
+                            color = Color.Gray
+                        )
+                    } else {
+                        LazyColumn {
+                            items(dogPictureList) { item ->
+                                PictureRow(url = item as String)
+                            }
                         }
                     }
+
+                    if (dogViewModel.searchForTypeResult is ResultState.Error) {
+                        val context = LocalContext.current
+
+                        Toast.makeText(
+                            context,
+                            dogViewModel.searchForTypeResult.message
+                                ?: context.getString(R.string.an_error_has_occurred),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
+            }
 
-                if (dogViewModel.searchForTypeResult is ResultState.Error) {
-                    val context = LocalContext.current
+            val focusManager = LocalFocusManager.current
 
-                    Toast.makeText(
-                        context,
-                        dogViewModel.searchForTypeResult.message ?: context.getString(R.string.an_error_has_occurred),
-                        Toast.LENGTH_SHORT
-                    ).show()
+            if(showDropDown) {
+                LazyColumn {
+                    items(dogViewModel.filteredDogBreedList) { item ->
+                        Divider(color = Color.White)
+                        Text(
+                            text = item.name,
+                            color = Color.White,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .clickable {
+                                    focusManager.clearFocus()
+                                    showDropDown = false
+                                    dogViewModel.searchForType(item.name)
+                                }
+                                .background(Purple_500)
+                                .padding(16.dp)
+                        )
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/dogpicturecompose/composelayouts/SearchAppBar.kt
+++ app/src/main/java/com/example/dogpicturecompose/composelayouts/SearchAppBar.kt
@@ -1,11 +1,7 @@
 package com.example.dogpicturecompose.composelayouts
 
-import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -14,15 +10,11 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dogpicturecompose.R
-import com.example.dogpicturecompose.db.DogBreed
 import com.example.dogpicturecompose.ui.theme.Purple_500
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,14 +22,11 @@ import com.example.dogpicturecompose.ui.theme.Purple_500
 fun SearchAppBar(
     onCloseIconClicked: () -> Unit,
     onSearchButtonClicked: (String) -> Unit,
-    onSearchDogBreeds: (String) -> Unit,
-    filteredDogBreedList: List<DogBreed>
+    onValueChange: (String) -> Unit,
 ) {
     var text by remember { mutableStateOf("") }
-    var showDropDown by remember { mutableStateOf(false) }
-    var searched by remember { mutableStateOf(false) }
 
-    Column(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
     ) {
@@ -48,10 +37,8 @@ fun SearchAppBar(
                 .padding(3.dp),
             value = text,
             onValueChange = { newText ->
-                Log.i("tag", "newText $newText")
                 text = newText
-                searched = false
-                onSearchDogBreeds(text)
+                onValueChange(text)
             },
             leadingIcon = {
                 IconButton(onClick = onCloseIconClicked) {
@@ -80,7 +67,6 @@ fun SearchAppBar(
             ),
             keyboardActions = KeyboardActions(
                 onSearch = {
-                    searched = true
                     onSearchButtonClicked(text)
                 }
             ),
@@ -91,30 +77,5 @@ fun SearchAppBar(
                 )
             }
         )
-
-        showDropDown = filteredDogBreedList.isNotEmpty() && text.isNotEmpty() && searched == false
-        val focusManager = LocalFocusManager.current
-
-        if(showDropDown) {
-            LazyColumn {
-                items(filteredDogBreedList) { item ->
-                    Text(
-                        text = item.name,
-                        color = Purple_500,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                text = item.name
-                                searched = true
-                                focusManager.clearFocus()
-
-                                onSearchButtonClicked(item.name)
-                            }
-                            .padding(16.dp)
-                    )
-                    Divider(color = Purple_500)
-                }
-            }
-        }
     }
 }
